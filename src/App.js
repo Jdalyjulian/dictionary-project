cc4d4674835e3af44aeb1f1ebf068f5f04920dd9
Jdalyjulian <jdalyjulian@gmail.com>
2023-07-24 12:49:43 -0500

    created dictionary form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
+import Dictionary from "./Dictionary"
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       Hello
-      </header>
+      <div className="container">
+        <header className="App-header">Dictionary</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer"><small>Coded by Julia Julian</small></footer>
+      </div>
     </div>
   );
 }
-
-export default App;
