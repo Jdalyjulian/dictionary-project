cd5de4a24be737a19e1fe581a2726192f30d754b
Jdalyjulian <jdalyjulian@gmail.com>
2023-07-25 13:05:59 -0500

    Added phonetics

--- src/Meaning.js
+++ src/Meaning.js
@@ -7,12 +7,10 @@ export default function Meaning (props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      <p>
         <strong>Definition: </strong>{props.meaning.definition}
         <br />
         <Example example={props.meaning.example} />
         <Synonyms synonyms={props.meaning.synonyms} />
-      </p>
     </div>
   )
 } 
---
+++ src/Phonetics.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Phonetics (props) {
+  console.log(props.phonetics);
+  return (
+    <div className="Phonetics">
+      <h3>{props.phonetics}</h3>
+    </div>
+  )}
